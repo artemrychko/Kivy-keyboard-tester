2270694e16b4ec4872b21f75945d4ae666add249
Alexander Taylor <alexanderjohntaylor@gmail.com>
2017-04-08 01:20:15 +0100

    Made attachment sending work

--- keyboardtester/main.py
+++ keyboardtester/main.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -6,6 +7,9 @@ from kivy.uix.label import Label
 from kivy.utils import platform
 from kivy.properties import (StringProperty, ListProperty,
                              NumericProperty)
+
+from kivy.core.window import Window
+Window.softinput_mode = 'pan'
 
 import sys
 from os import unlink
@@ -18,6 +22,70 @@ if platform =='android':
 # the desktop
 if abspath('.') not in sys.path:
     sys.path.append(abspath('.'))
+
+from scrolllabel import ScrollLabel
+
+if platform == 'android':
+    from jnius import autoclass
+    Environment = autoclass('android.os.Environment')
+    activity = autoclass('org.kivy.android.PythonActivity').mActivity
+    # directory = Environment.getExternalStorageDirectory(None)
+    directory = activity.getExternalFilesDir(None)
+    filename = join(directory.getAbsolutePath(), 'keyboard_log.txt')
+else:
+    filename = abspath('keyboard_log.txt')
+
+print('filename is', filename)
+
+def send_email(text, filen):
+    print('Asked to send email with text {} and file {}'.format(text, filen))
+    if platform == 'android':
+        send_email_android(text, filen)
+    else:
+        import webbrowser
+        webbrowser.open('test')
+
+def send_email_android(text, filen):
+    from jnius import autoclass, cast
+
+    Intent = autoclass('android.content.Intent')
+    AndroidString = autoclass('java.lang.String')
+    Uri = autoclass('android.net.Uri')
+
+    activity = autoclass('org.kivy.android.PythonActivity').mActivity
+
+    intent = Intent(Intent.ACTION_SEND)
+    intent.setType('message/rfc822')
+
+    recipient = 'alexanderjohntaylor@gmail.com'
+    subject = 'Kivy keyboard log'
+
+    intent.putExtra(Intent.EXTRA_EMAIL, [recipient])
+
+    android_subject = cast('java.lang.CharSequence',
+                            AndroidString(subject))
+    intent.putExtra(Intent.EXTRA_SUBJECT, android_subject)
+
+    android_text = cast('java.lang.CharSequence',
+                        AndroidString(text))
+    intent.putExtra(Intent.EXTRA_TEXT, android_text)
+
+    # File = autoclass('java.io.File')
+    # file_object = File(filen)
+    # print('file is', file_object)
+    file_uri = Uri.parse('file://{}'.format(filen))
+    # file_uri = Uri.fromFile(file_object)
+    # print('uri is', file_uri)
+    intent.putExtra(Intent.EXTRA_STREAM, cast('android.os.Parcelable', file_uri))
+    # intent.putExtra(Intent.EXTRA_STREAM, Uri.parse('file://{}'.format(filen)))
+    # intent.putExtra(Intent.EXTRA_STREAM, 'file://{}'.format(filen))
+
+    chooser_title = cast('java.lang.CharSequence',
+                            AndroidString('Send message with:'))
+    activity.startActivity(Intent.createChooser(intent,
+                                                chooser_title))
+    # activity.startActivity(intent)
+
 
 class ColouredButton(ButtonBehavior, Label):
     background_normal = ListProperty([1, 1, 1, 1])
@@ -39,7 +107,7 @@ class DeviceInfoScreen(Screen):
                         'enter text with your keyboard using the '
                         'specific method requested.')
 
-    no_output_text = ('Even if you see no keyboard output, keep typin g'
+    no_output_text = ('Even if you see no keyboard output, keep typing '
                       'as if the keyboard is working.')
     
     error_text = ('Don\'t worry if you make a mistake in any '
@@ -69,6 +137,10 @@ class DeviceInfoScreen(Screen):
         App.get_running_app().log('Model: {}'.format(self.model), prefix='  ')
         # App.get_running_app().log('Product: {}'.format(self.product))
 
+        App.get_running_app().body_text = (
+            'Keyboard log for brand {}, manufacturer {}, model {}.'.format(
+                self.brand, self.manufacturer, self.model))
+
 
 
 class TapInputScreen(Screen):
@@ -76,11 +148,11 @@ class TapInputScreen(Screen):
         App.get_running_app().log('SCREEN: tap input')
 
     def skip(self):
-        App.get_running_app().log('Next: SKIP', prefix='  ')
+        App.get_running_app().log('Result: SKIP', prefix='  ')
         self.next()
 
     def done(self):
-        App.get_running_app().log('Next: DONE', prefix='  ')
+        App.get_running_app().log('Result: DONE', prefix='  ')
         self.next()
 
     def next(self):
@@ -92,11 +164,11 @@ class SwipeInputScreen(Screen):
         App.get_running_app().log('SCREEN: swipe input')
 
     def skip(self):
-        App.get_running_app().log('Next: SKIP', prefix='  ')
+        App.get_running_app().log('Result: SKIP', prefix='  ')
         self.next()
 
     def done(self):
-        App.get_running_app().log('Next: DONE', prefix='  ')
+        App.get_running_app().log('Result: DONE', prefix='  ')
         self.next()
 
     def next(self):
@@ -107,32 +179,46 @@ class OtherInputScreen(Screen):
         App.get_running_app().log('SCREEN: other input')
 
     def skip(self):
-        App.get_running_app().log('Next: SKIP', prefix='  ')
+        App.get_running_app().log('Result: SKIP', prefix='  ')
         self.next()
 
     def done(self):
-        App.get_running_app().log('Next: DONE', prefix='  ')
+        App.get_running_app().log('Result: DONE', prefix='  ')
         self.next()
 
     def next(self):
-        App.get_running_app().root.current = 'otherinput'
+        App.get_running_app().root.current = 'final'
+
+class FinalScreen(Screen):
+    file_text = StringProperty()
+
+    def on_pre_enter(self):
+        App.get_running_app().log('SCREEN: final')
+
+        with open(filename, 'r') as fileh:
+            self.file_text = fileh.read()
 
 
 class KeyboardTesterApp(App):
+    body_text = StringProperty()
+
     def build(self):
         self.reset_log()
         return Manager()
 
     def reset_log(self):
-        if exists('keyboard_log.txt'):
-            unlink('keyboard_log.txt')
+        if exists(filename):
+            unlink(filename)
 
     def log(self, s, newline=True, prefix=''):
         if newline and not s.endswith('\n'):
             s = s + '\n'
         s = prefix + s
-        with open('keyboard_log.txt', 'a') as fileh:
+        with open(filename, 'a') as fileh:
             fileh.write(s)
+
+    def send_email(self):
+        send_email(self.body_text, abspath(filename))
 
 
 
